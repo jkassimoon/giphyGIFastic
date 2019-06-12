2a0eed3702abd6c169636f3295067e2749ba2ddc
thepickupkitten <jkashmoon@gmail.com>
2019-06-11 17:39:03 -0700

    initial set up

---
+++ assets/javascript/script.js
@@ -0,0 +1,12 @@
+var topics=[];
+
+for (var i = 0; i < 10 ; i++) {
+    var moodButton = $("<button>");
+    moodButton.addClass("mood-button");
+    // moodButton.attr("src", "https");
+    $(".buttons-div").append(moodButton);
+};
+
+$(".mood-button").on("click", function() {
+
+});
